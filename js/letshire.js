463bd6ec2a797dcf7b2b9ad32034aea848561c0c
stephenhu <hus@vmware.com>
2013-11-01 04:43:40 +0800

    initial import

---
+++ js/letshire.js
@@ -0,0 +1,18 @@
+function openingDialog() {
+
+  $("#opening-dialog").dialog("open");
+
+} // openingDialog
+
+function resumeDialog() {
+
+  $("#resume-dialog").dialog("open");
+
+} // resumeDialog
+
+function recommendDialog() {
+
+  $("#recommend-dialog").dialog("open");
+
+} // recommendDialog
+
